9542e7669913ddcebadc619b12240148480d851a
axefirm <axefirm1@gmail.com>
2019-05-11 20:32:38 +0800

    second

---
+++ frontend/pages/index.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <v-layout class="container">
+    <nuxt-link to="/login" class="text item">Login</nuxt-link>
+    <nuxt-link to="/signup" class="text item">Signup</nuxt-link>
+  </v-layout>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+  .text{
+    text-decoration: none;
+    color: black;
+    font-size: 28px;
+  }
+  .item{
+    background-color: cyan;
+    float: left;
+    border: 1px;
+  }
+</style>
---
+++ frontend/pages/menu.vue
@@ -0,0 +1,88 @@
+<template lang="html">
+  <div class="container">
+    <div class="header">
+      <div class="nav">
+        <ul >
+        <li><img src="~/assets/menu/kt.png" alt="" class="logo"></li>
+        <li><nuxt-link class="link text" to="/next">Дараа улирал</nuxt-link></li>
+        <li><nuxt-link class="link text" to="/all"> Бүх улирал</nuxt-link></li>
+        </ul>
+      </div>
+    </div>
+    <div class="select">
+      <v-flex xs12 sm6 md6>
+        <v-flex xs12 sm6 d-flex>
+          <v-select
+          :items="program"
+          label="Хөтөлбөр"
+          outline
+          v-model="semestr"
+          ></v-select>
+        </v-flex>
+      </v-flex>
+    <v-flex xs12 sm6 md6>
+      <v-flex xs12 sm6 d-flex>
+        <v-select
+        :items="items"
+        label="Төгсөхийг хүссэн улирал"
+        outline
+        v-model="semestr"
+        ></v-select>
+      </v-flex>
+     </v-flex>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      program:['blah','blahh'],
+      semestr:"",
+      items:['6','7','8','9'],
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  .header{
+    height: 100px;
+    width: 100%;
+  }
+  .logo{
+    height: 100px;
+  }
+  .nav{
+    height: 100%;
+  }
+  .nav ul{
+    height: 100%;
+  }
+  .nav ul li{
+    float:left;
+    list-style: none;
+    height: 100%;
+    display: flex;
+    flex-direction:column;
+    justify-content: center;
+    align-items: center;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+  .link{
+    text-decoration: none;
+    color: black;
+  }
+  .text{
+    font-size: 16px;
+    font-weight: 700;
+    font-family:  'Roboto Condensed', sans-serif;
+  }
+  .nav ul li:first-child{
+    padding-left: 0px;
+  }
+
+</style>
